ddc3583e8968feaf034000a11becedcefff0544c
ckrgz001 <krcgonzales@gmail.com>
2018-02-04 17:42:35 +0000

    Create new template, action, route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
 
   def new
-    
-    
+    @article = Article.new
   end
+  
+  def create
+    @article = Article.new(article_params) #create an aritcle based on passed in params/ see private method
+    @article.save #save it
+    redirect_to articles_show(@article)
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :description) #article is top level key, this permits the values (as in key-value pairings) of title and description
+    end
   
   
 end
