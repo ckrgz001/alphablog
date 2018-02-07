bca085b7855b8e8ed97b9095e67e8cadc98d1d3f
ckrgz001 <krcgonzales@gmail.com>
2018-02-07 02:56:03 +0000

    Fixed signin on signup issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user_id
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to AlphaBlog, #{@user.username.upcase}!"
       redirect_to user_path(@user)
     else
